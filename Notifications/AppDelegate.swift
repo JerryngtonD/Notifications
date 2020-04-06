351f5eb20889f84a44708c44e34c2f77d75268de
jerryngton <jerryngtond@gmail.com>
2020-04-06 21:49:49 +0300

    scheduled notification

---
+++ Notifications/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  Notifications
+//
+//  Created by Alexey Efimov on 21.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    let notificationCenter = UNUserNotificationCenter.current()
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        requestAuthorization()
+        return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+    }
+    
+    func requestAuthorization() {
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Permission granted\(granted)")
+            guard granted else { return }
+            self.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        notificationCenter.getNotificationSettings { (settings) in
+            print("Notification settings \(settings)")
+        }
+    }
+    
+    func scheduleNotification(notificationType: String) {
+          let content = UNMutableNotificationContent()
+        
+        content.title = notificationType
+        content.body = "This is example how to create \(notificationType)"
+        content.sound = .default
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let identidier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identidier, content: content, trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+    }
+}
+                                                                                                    
